f69cd1b0b48be15ad81a2726531e131fc50c6c22
Oscar Leonel Segura Valencia <SV20008@ti.ues.edu.sv>
2021-04-18 22:21:29 -0600

    Agregue clase al ejercicio

---
+++ src/Clases_creadas/TRIANGULO.java
@@ -0,0 +1,46 @@
+
+package Clases_creadas;
+
+public class TRIANGULO {
+    private double lado1;
+    private double lado2;
+    private double lado3;
+
+    public double getLado1() {
+        return lado1;
+    }
+
+    public double getLado2() {
+        return lado2;
+    }
+
+    public double getLado3() {
+        return lado3;
+    }
+    
+    public void setLado1(double lado1) {
+        this.lado1 = lado1;
+    }
+
+    public void setLado2(double lado2) {
+        this.lado2 = lado2;
+    }
+
+    public void setLado3(double lado3) {
+        this.lado3 = lado3;
+    }
+    
+    
+    
+    public String MostrarResultado(){       
+        if (lado1 == lado2 && lado3==lado2 && lado3==lado1 ) {
+            return "Triangulo Equilatero";               
+
+        }else if (lado1==lado2 || lado2==lado3 || lado3==lado1){
+            return "Triangulo Isosceles";
+        }
+        else{
+             return "Triangulo Escaleno";
+        }
+    }   
+}
